0ad3812620831643723baa220e4a8ea2f643340c
Xavier Taylor <tayloxav002@gmail.com>
2024-09-26 08:33:12 -0400

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+droids= input("how many droids do you want to meet:")
+wookiees= input( "how many wookiees do you want to meet:")
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
